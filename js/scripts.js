57860fefafff939e3ea260564f5db0b9b983dae1
Ariel Vito <66864947+Pixelshiftx@users.noreply.github.com>
2022-01-28 15:41:39 +0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 /*!
-* Start Bootstrap - Stylish Portfolio v6.0.1 (https://startbootstrap.com/theme/stylish-portfolio)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Stylish Portfolio v6.0.1 (https://startbootstrap.com/theme/stylish-portfolio)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
+ */
 window.addEventListener('DOMContentLoaded', event => {
 
     const sidebarWrapper = document.getElementById('sidebar-wrapper');
@@ -79,103 +79,204 @@ function fadeIn(el, display) {
     })();
 };
 
-document.body.addEventListener("mousemove", evt => {
-    const mouseX = evt.clientX;
-    const mouseY = evt.clientY;
-    
-    gsap.set(".cursor", {
-      x: mouseX,
-      y: mouseY
-    })
-    
-    gsap.to(".shape", {
-      x: mouseX,
-      y: mouseY,
-      stagger: -0.1
-    })
-  })
 
-  
-  
 
+
+
+
+
+function time() {
+    var clock = document.getElementById('spanClock');
+    var d = new Date();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    clock.textContent =
+        ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+}
+setInterval(time, 1000);
+
+var DateCont = document.getElementById('currDate');
+var date = new Date();
+var day;
+var month;
+switch (date.getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
+switch (date.getMonth()) {
+    case 0:
+        month = "January";
+        break;
+    case 1:
+        month = "February";
+        break;
+    case 2:
+        month = "March";
+        break;
+    case 3:
+        month = "April";
+        break;
+    case 4:
+        month = "May";
+        break;
+    case 5:
+        month = "June";
+        break;
+    case 6:
+        month = "July";
+    case 7:
+        month = "August";
+        break;
+    case 8:
+        month = "September";
+        break;
+    case 9:
+        month = "October";
+        break;
+    case 10:
+        month = "November";
+        break;
+    case 11:
+        month = "December";
+        break;
+}
+DateCont.textContent = day + ", " + date.getDate() + " " + month + " " + date.getFullYear();
+
+/*
+var data =
+'[ { "name": "Aragorn", "race": "Human" }, { "name": "Gimli", "race": "Dwarf" } ]'
+
+data = JSON.parse(data);
+for (var i=0;i<data.length;i++)
+{
+  console.log('Name: '+ data[i].name + ' and Race: '+data[i].race +'.')
+}
+      
+var request = new XMLHttpRequest();
+var titleInsert =  "Monogatari"
+request.open('GET', 'https://api.jikan.moe/v3/search/anime?q='+titleInsert+'&page=1');
+      
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    var obj = JSON.parse(this.responseText);
     
-    function time() {
-        var clock = document.getElementById('spanClock');
-        var d = new Date();
-        var s = d.getSeconds();
-        var m = d.getMinutes();
-        var h = d.getHours();
-        clock.textContent = 
-          ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
-      }
-      setInterval(time, 1000);
-
-    var DateCont = document.getElementById('currDate');
-    var date = new Date();
-    var day;
-    var month;
-    switch (date.getDay()) {
-        case 0:
-          day = "Sunday";
-          break;
-        case 1:
-          day = "Monday";
-          break;
-        case 2:
-           day = "Tuesday";
-          break;
-        case 3:
-          day = "Wednesday";
-          break;
-        case 4:
-          day = "Thursday";
-          break;
-        case 5:
-          day = "Friday";
-          break;
-        case 6:
-          day = "Saturday";
-      }
-      switch (date.getMonth()) {
-        case 0:
-          month = "January";
-          break;
-        case 1:
-          month = "February";
-          break;
-        case 2:
-           month = "March";
-          break;
-        case 3:
-          month = "April";
-          break;
-        case 4:
-          month = "May";
-          break;
-        case 5:
-          month = "June";
-          break;
-        case 6:
-          month = "July";
-          case 7:
-            month = "August";
-            break;
-          case 8:
-            month = "September";
-            break;
-          case 9:
-             month = "October";
-            break;
-          case 10:
-            month = "November";
-            break;
-          case 11:
-            month = "December";
-            break;
-      }
-      DateCont.textContent= day +", "+ date.getDate()+ " "+month +" "+  date.getFullYear();
+    
+    for (var i=0;i<10;i++)
+    {
      
+      console.log('Name: '+obj[i].title+' ID: '+obj[i].mal_id+'.');
+    }
     
-    
-    
-     
+  }
+  
+  for (var i = 0; i < 10; i++) {
+      var counter = obj.results[i];
+      console.log('Title :'+counter.title+' and ID: '+counter.mal_id);
+      console.log('Synopsis :'+counter.synopsis);
+    }
+};
+request.send();
+*/
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+};
+
+const elementOutofView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+};
+
+const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+};
+
+
+
+const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+            displayScrollElement(el);
+        }
+    })
+}
+
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+});
+
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function() {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function(responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
